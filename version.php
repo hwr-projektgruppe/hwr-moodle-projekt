864fb18bf006be307e825500af05a82a3f459567
m4ximumpow3r <s_woityczka@stud.hwr-berlin.de>
2015-07-08 00:07:50 +0200

    Finale Version Alarmplugin

--- version.php
+++ version.php
@@ -1,14 +1,5 @@
 <?php
-global $CFG;
-//$xml = simplexml_load_file(dirname(dirname(dirname(__FILE__))) ."/blocks/alarm/settings.xml");
-$xml = simplexml_load_file($CFG->dirroot ."/blocks/alarm/settings.xml");
-if($xml !== FALSE){
-    $cronjobDays = (int)$xml->cronjob;
-    $version = (int)$xml->version;
-}
-else{
-    exit("settings.xml nicht gefunden!");
-}
-$plugin->version = $version; // YYYYMMDDHH (year, month, day, 24-hr time)
+$cronjobHours = 12;
+$plugin->version = 2015070121; // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
-$plugin->cron = $cronjobDays * 24 * 60 * 60; // Seconds.
+$plugin->cron = $cronjobHours * 60 * 60; // Seconds.
